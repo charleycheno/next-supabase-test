9e62cea2c1bf77a72500b4d00a2af507c527e8b8
Charley <charleychen2006@gmail.com>
2024-06-23 16:16:31 +0200

    Fix bugs

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY
+const supabaseUrl = "https://eairauklnrffrdnymanp.supabase.co"
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVhaXJhdWtsbnJmZnJkbnltYW5wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTg5MDk1OTAsImV4cCI6MjAzNDQ4NTU5MH0.YmFUxboYz9HHa6Y1KbmDk6LRZWXCWmbhhI9DhKKl46s"
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
